808ef6bfffcc83925abbb576f5f1ee11853afeb4
sparethme <spare@ualberta.ca>
2020-12-02 22:50:51 -0500

    useState and app styling

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Name() {
+
+  const [name, setName] = useState('');
+
   return (
-    <div>Hello</div>
+    <label>
+      <input className="header-name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        onClick={e => e.target.setSelectionRange(0, e.target.value.length)}
+        placeholder={'Untitled'}
+      >
+      </input>
+    </label>
   );
 };
